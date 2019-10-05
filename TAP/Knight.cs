b142d17130ca18cebb04f5cdf3ffc314bd8e0e81
sarahrolandc <sarahrolandc@outlook.com>
2019-10-04 22:02:37 -0300

    commit inicial

---
+++ TAP/Knight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TAP
+{
+    class Knight
+    {
+        public int score { get; private set; }
+        public int effort { get; private set; }
+
+        public Knight(int score, int effort)
+        {
+            this.score = score;
+            this.effort = effort;
+        }
+    }
+}
